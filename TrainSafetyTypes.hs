7cd15095ae4542c375692c2ad22ea256b6fb5738
Luke Borwell <lborwell@gmail.com>
2014-01-16 09:42:13 +0000

    Change direction messages

--- TrainSafetyTypes.hs
+++ TrainSafetyTypes.hs
@@ -4,13 +4,17 @@ import qualified Data.Map as Map
 
 type Layout = Map.Map String Section
 type SensorID = String
-data Direction = FWD | BKW deriving (Show)
+data Direction = FWD | BKW deriving (Show, Eq)
 data State = Justleft | Justentered | Empty | Occupied deriving (Show, Eq)
 data SensorUpdate = Hi | Low deriving (Show, Eq)
 
 data SpeedMessage = SpeedMessage { fromslot :: Int
 								 , newspeed :: Int
 								 } deriving (Show)
+
+data DirectionMessage = DirectionMessage { dirslot :: Int
+										 , newdir :: Direction 
+										 } deriving (Show, Eq)
 
 data Section = Section { state :: State
 					   , prev :: [SensorID]
--- trainsafety.hs
+++ trainsafety.hs
@@ -5,10 +5,20 @@ import qualified Data.Map as Map
 --import Debug.Trace
 
 process :: [String] -> Layout -> [String]
-process inp@("A0":as) t = makeSafe $ speedChange t (parseSpeed inp)
+process i t = makeSafe (parseInput i t)
+
+parseInput :: [String] -> Layout -> Layout
+parseInput inp@("A0":as) t = speedChange t (parseSpeed inp)
 
 makeSafe :: Layout -> [String]
 makeSafe t = checkSpeeds t
+
+
+-------------------------------------------------
+--
+--       Slow Down Speeding Trains
+--
+-------------------------------------------------
 
 checkSpeeds :: Layout -> [String]
 checkSpeeds t = slowSpeeds (Map.elems speeders)
@@ -18,6 +28,13 @@ checkSpeeds t = slowSpeeds (Map.elems speeders)
 slowSpeeds :: [Section] -> [String]
 slowSpeeds [] = []
 slowSpeeds (t:ts) = ("0 " ++ show (slot (loco t)) ++ " " ++ show (speedlim t)) : (slowSpeeds ts)
+
+
+-------------------------------------------------
+--
+--       Incoming Speed Message
+--
+-------------------------------------------------
 
 parseSpeed :: [String] -> SpeedMessage
 parseSpeed inp = SpeedMessage { fromslot=x, newspeed=y }
@@ -36,6 +53,23 @@ speedChange t m = Map.insert (sid sec) (sec { loco=((loco sec) { speed=(newspeed
 findLoco :: Layout -> Int -> Section
 findLoco t s = head (Map.elems (Map.filter (\x -> (slot (loco x)) == s) t))
 
+
+-------------------------------------------------
+--
+--       Incoming Direction Change Message
+--
+-------------------------------------------------
+
+changeDirection :: Layout -> DirectionMessage -> Layout
+changeDirection t m = Map.insert (sid sec) (sec { loco=((loco sec) { direction=(newdir m) })}) t
+	where sec = findLoco t (dirslot m)
+
+
+-------------------------------------------------
+--
+--       Incoming Section Boundary Message
+--
+-------------------------------------------------
 -- On sensor trigger:
 -- If sensor goes high, check prev/next sections for Justleft
 -- If justleft, take its locomotive, set this to occupied, neighbour to empty
@@ -54,18 +88,17 @@ checkNextEntered :: Layout -> Section -> Layout
 checkNextEntered t s@(Section { next=[] })  = checkPrevEntered t (t Map.! (sid s))
 checkNextEntered t s@(Section { next=(n:ns) }) | state (t Map.! n) == Justleft = moveLoco t (t Map.! n) (t Map.! (sid s))
 											   | otherwise = checkNextEntered t (s { next=ns })
-																			
 
 checkPrevEntered :: Layout -> Section -> Layout
 checkPrevEntered t s@(Section { prev=[] }) = Map.insert (sid s) ((t Map.! (sid s)) { state=Justentered }) t
 checkPrevEntered t s@(Section { prev=(n:ns) }) | state (t Map.! n) == Justleft = moveLoco t (t Map.! n) (t Map.! (sid s))
 											   | otherwise = checkPrevEntered t (s { prev=ns })
 
+
 checkNextExited :: Layout -> Section -> Layout
 checkNextExited t s@(Section { next=[] })  = checkPrevExited t s
 checkNextExited t s@(Section { next=(n:ns) }) | state (t Map.! n) == Justentered = moveLoco t (t Map.! (sid s)) (t Map.! n)
 											  | otherwise = checkNextExited t (s { next=ns })
-																			
 
 checkPrevExited :: Layout -> Section -> Layout
 checkPrevExited t s@(Section { prev=[] }) = Map.insert (sid s) ((t Map.! (sid s)) { state=Justleft }) t
