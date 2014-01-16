9db743e1d1f91c54ddb54d306bfebb67ae0509dd
groundlicker <lborwell@gmail.com>
2014-01-16 06:58:03 +0000

    Generate speed limit messages

---
+++ TrainSafetyTypes.hs
@@ -0,0 +1,27 @@
+module TrainSafetyTypes where
+
+import qualified Data.Map as Map
+
+type Layout = Map.Map String Section
+type SensorID = String
+data Direction = FWD | BKW deriving (Show)
+data State = Justleft | Justentered | Empty | Occupied deriving (Show, Eq)
+data SensorUpdate = Hi | Low deriving (Show, Eq)
+
+data SpeedMessage = SpeedMessage { fromslot :: Int
+								 , newspeed :: Int
+								 } deriving (Show)
+
+data Section = Section { state :: State
+					   , prev :: [SensorID]
+					   , next :: [SensorID]
+					   , speedlim :: Int
+					   , loco :: Locomotive
+					   , sid :: SensorID
+					   } deriving (Show)
+
+data Locomotive = Locomotive { slot :: Int
+							 , speed :: Int
+							 , ide :: Int
+							 , direction :: Direction
+							 } deriving (Show)
--- trainsafety.hs
+++ trainsafety.hs
@@ -1,85 +1,33 @@
+import Testtracks
+
+import TrainSafetyTypes
 import qualified Data.Map as Map
 --import Debug.Trace
 
-type Layout = Map.Map String Section
-type SensorID = String
-data Direction = FWD | BKW deriving (Show)
-data State = Justleft | Justentered | Empty | Occupied deriving (Show, Eq)
-data SensorUpdate = Hi | Low deriving (Show, Eq)
+process :: [String] -> Layout -> [String]
+process inp@("A0":as) t = makeSafe $ speedChange t (parseSpeed inp)
 
-data SpeedMessage = SpeedMessage { fromslot :: Int
-								 , newspeed :: Int
-								 } deriving (Show)
+makeSafe :: Layout -> [String]
+makeSafe t = checkSpeeds t
 
-data Section = Section { state :: State
-					   , prev :: [SensorID]
-					   , next :: [SensorID]
-					   , speedlim :: Int
-					   , loco :: Locomotive
-					   , sid :: SensorID
-					   } deriving (Show)
+checkSpeeds :: Layout -> [String]
+checkSpeeds t = slowSpeeds (Map.elems speeders)
+	where	speeders = Map.filter speeding t
+		speeding (Section { speedlim=sl, loco=l }) = if (speed l) > sl then True else False
 
-data Locomotive = Locomotive { slot :: Int
-							 , speed :: Int
-							 , ide :: Int
-							 , direction :: Direction
-							 } deriving (Show)
+slowSpeeds :: [Section] -> [String]
+slowSpeeds [] = []
+slowSpeeds (t:ts) = ("0 " ++ show (slot (loco t)) ++ " " ++ show (speedlim t)) : (slowSpeeds ts)
 
-test1 :: Section
-test1 = Section { state=Occupied, prev=["T2"], next=[], speedlim=113, loco=testLoco, sid="T1" }
+parseSpeed :: [String] -> SpeedMessage
+parseSpeed inp = SpeedMessage { fromslot=x, newspeed=y }
+	where (_:x:y:_) = readinp (makeHex inp)
 
-test2 :: Section
-test2 = Section { state=Empty, prev=[], next=["T1"], speedlim=113, loco=noloco, sid="T2" }
+readinp :: [String] -> [Int]
+readinp r = map read r
 
-test3 :: Section
-test3 = Section { state=Occupied, prev=["T4"], next=[], speedlim=113, loco=testLoco, sid="T3" }
-
-test4 :: Section
-test4 = Section { state=Empty, prev=[], next=["T3"], speedlim=113, loco=noloco, sid="T4" }
-
-testTrack :: [(String,Section)]
-testTrack = [("T1",test1),("T2",test2),("T3",test3),("T4",test4)]
-
-testDict :: Layout
-testDict = Map.fromList testTrack
-
-
-noloco :: Locomotive
-noloco = Locomotive { slot=0, speed=0, ide=0, direction=FWD }
-
-testLoco :: Locomotive
-testLoco = Locomotive { slot=999, speed=113, ide=2, direction=FWD }
-
-a1 :: Section
-a1 = Section { state=Occupied, prev=["D1"], next=["B1"], speedlim=113, loco=testLoco, sid="A1" }
-
-a2 :: Section
-a2 = Section { state=Empty, prev=["D1","D2"], next=["B2"], speedlim=113, loco=noloco, sid="A2" }
-
-b1 :: Section
-b1 = Section { state=Empty, prev=["A1"], next=["C1"], speedlim=113, loco=noloco, sid="B1" }
-
-b2 :: Section
-b2 = Section { state=Empty, prev=["A2"], next=["B2"], speedlim=113, loco=noloco, sid="B2" }
-
-c1 :: Section
-c1 = Section { state=Empty, prev=["B1","B2"], next=["D1"], speedlim=113, loco=noloco, sid="C1" }
-
-c2 :: Section
-c2 = Section { state=Empty, prev=["B2"], next=["D2"], speedlim=113, loco=noloco, sid="C2" }
-
-d1 :: Section
-d1 = Section { state=Empty, prev=["C1"], next=["A1","A2"], speedlim=113, loco=noloco, sid="D1" }
-
-d2 :: Section
-d2 = Section { state=Empty, prev=["C2"], next=["A2"], speedlim=113, loco=noloco, sid="D2" }
-
-track :: [(String, Section)]
-track = [("A1",a1),("A2",a2),("B1",b1),("B2",b2),("C1",c1),("C2",c2),("D1",d1),("D2",d2)]
-
-trackDict :: Layout 
-trackDict = Map.fromList track
-
+makeHex :: [String] -> [String]
+makeHex st = map (\x -> "0x" ++ x) st
 
 speedChange :: Layout -> SpeedMessage -> Layout
 speedChange t m = Map.insert (sid sec) (sec { loco=((loco sec) { speed=(newspeed m) }) }) t
@@ -129,3 +77,4 @@ moveLoco t from to = clearSection (Map.insert (sid to) (to {state=Occupied,loco=
 
 clearSection :: Layout -> Section -> Layout
 clearSection t s = Map.insert (sid s) (s {state=Empty, loco=noloco}) t
+
